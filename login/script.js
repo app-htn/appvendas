397961f3f4dba47194a119f25f51441f6fc43beb
Alisson Hiosaki <92666141+AlissonHiosaki@users.noreply.github.com>
2024-09-27 23:02:12 -0300

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,15 +1,13 @@
 function logar(){
 
-    var login = document.getElementById('login').value;
-    var senha = document.getElementById('senha').value;
+     var login = document.getElementById('usuario').value;
+     var senha = document.getElementById('senha').value;
+ 
+     if(login == "admin" && senha == "admin"){
 
-    if(login == "admin" && senha == "admin"){
-    if(login == "vm42" && senha == "vmdl"){
-    if(login == "estoque42" && senha == "Huma42"){
-    if(login == "Alisson.Hiosaki" && senha == "39633"){
-        
-        location.href = "https://app-htn.github.io/appvendas/relatorio/relatorio.html";
-   }else{
-        alert('Usuario ou senha incorreta')
-   }
-}
+         
+         location.href = "https://app-htn.github.io/appvendas/relatorio/relatorio.html";
+    }else{
+         alert('Usuario ou senha incorreta')
+    }
+ }
